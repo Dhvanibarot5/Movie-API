d95de0c0a99ebe4cbcd2b8d84532089d1356f5a2
Dhvanibarot5 <dhvanibarot5@gmail.com>
2024-11-21 18:13:46 +0530

    first commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = 'your_tmdb_api_key'; // Get this from TMDB website
+const API_KEY = 'your_tmdb_api_key'; 
 const BASE_URL = 'https://api.themoviedb.org/3';
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
